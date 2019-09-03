6a592aa382071595bed7197fdba4259535718ca4
DIckY CaNDRA <52904090+dizart-y@users.noreply.github.com>
2019-09-04 01:23:28 +0700

    Create StabilSpeed.py

---
+++ StabilSpeed.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+#Hiya Ini Program Jelek :k
+#Hiya W Udah Tau Ini Program Jelek :D
+#Jelek Jelek Gini Nih Pasti Di Recode:)
+#Awas Eror ^_^
+#Yammaap Kalau Jelek Namanya Juga Newbie :k
+
+import os
+import time
+import sys
+os.system('clear')
+os.system('reset')
+print ("\033[1;91m___  ____ ____ _  _    ____ ___  ____ ____ ___  \033[1;91m")
+print ("\033[1;97m|  \ |__| |__/ |_/     [__  |__] |___ |___ |  \  \033[1;97m")
+print ("\033[1;91m|__/ |  | |  \ | \_    ___] |    |___ |___ |__/  v7.4\n\033[1;91m") 
+print("\033[1;97m●❯────────────────────────────────────❮●")
+print("\033[1;91mAuthor    : PuTRA DiCkO")
+print("\033[1;97mGitHub   : https//github.com/dizart-y")
+print("\033[1;91mSupport : /.Volta|DIckyCaNdRA|Edi")
+print("\033[1;97m●❯────────────────────────────────────❮●\n")
+print("\033[1;97m Ini Program Membantu Untuk Stabilkan Sinyal")
+print("\033[1;97mDan Juga Melancarkan Speed Kouta \n")
+
+enter = input('\033[3;92mTekan Enter Untuk Mulai...')
+# Menjalankan Agar Bisa Sinyal Berjalan #
+def jalan():
+          jalan = enter
+          os.system('clear')
+          D_I_Z_A_R_T_Y()
+          time.sleep(7)
+# Sedang Mencari Sinyal Agar Sinyal Stabil #
+def sinyaal():
+            sinyall = speed
+            os.system('clear')
+            D_I_Z_A_R_T_Y()
+            time.sleep(7)
+# Sedang Mencari Sinyal Agar Sinyal Menjadi Speed #
+def speed():
+          speed = sinyal
+          os.system('clear')
+          D_I_Z_A_R_T_Y()
+          time.sleep(7)
+# Sedang Melakukan Proses Sinyal + Menjadi Speed #
+def otw():
+         otw = Sinyal + Stabill
+         otw = Sinyall + Speed
+         zart = zart
+         dizarty - dizarty
+         os.system('clear')
+         D_I_Z_A_R_T_Y()
+         time.sleep(7)
+# Sedang Scan Sinyal #
+def scanner():
+         		melakukan = scanner
+         		melakukan = proses - sinyal - lemah
+         		melakukan = proses = scanning = sinyal - speed + kouta
+         		os.system('clear')
+         		time.sleep(7)
+# Sedang Mestabilkan Sinyal Agar Sinyal Lancar #
+def mestabilkan():
+                 			mestabilkan = sinyal
+                 			mestabilkan = jaringan = tempat
+                 			mestabilkan = sinyal =pas
+                 			opwening, lord - sinyal < stabil 
+                 			D_I_Z_A_R_T_Y()
+                 			time.sleep(7)
+#Sedang Tracy Melakukan Opwning Get Get Sinyal #
+def tracy():
+             ownres = get + get
+             D_I_Z_A_R_T_Y()
+             time.sleep(7)
+time.sleep(7)
+os.system("xdg-open https://api.whatsapp.com/send?phone=62881026885233")
+os.system("xdg-open https://api.whatsapp.com/send?phone=6285321421228")
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping sinyal.com')
+os.system('ping speed.com')
+
+def back():
+       back = awal - menu
+       owning = true - sys
